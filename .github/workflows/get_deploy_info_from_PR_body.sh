8d95e6be85f7e7bf428e2b5931e5574c483e26da
edgarulg <edgar.garcia@armory.io>
2021-04-05 17:03:52 -0500

    fix(workflow): move the PR body extractor logic to a single script

---
+++ .github/workflows/get_deploy_info_from_PR_body.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+export PR_BODY="$1"
+
+eventData=$(echo "$PR_BODY" | sed '/^{/,/^}/!d')
+name=$(echo $eventData | jq '.service.name')
+baseService=$(echo $eventData | jq '.service.details.baseService')
+image=$(echo $eventData | jq '.service.details.image.repository')
+tag=$(echo $eventData | jq '.service.details.image.tag')
+if [ ! -z "$baseService" -a "$baseService" = "null" ]; then
+  echo "No baseService property found, default to name property"
+  echo "##[set-output name=serviceName;]$name"
+else
+  echo "using baseService property as serviceName"
+  echo "##[set-output name=serviceName;]$baseService"
+fi
+echo "##[set-output name=image;]$image"
+echo "##[set-output name=tag;]$tag"
