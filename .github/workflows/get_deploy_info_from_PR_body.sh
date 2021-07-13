70e2ea2c18835f04affc01016c3c2184ec0af889
edgarulg <edgar.garcia@armory.io>
2021-07-13 09:15:41 -0500

    fix(deploy): support extract name from baseService & baseVcs.

--- .github/workflows/get_deploy_info_from_PR_body.sh
+++ .github/workflows/get_deploy_info_from_PR_body.sh
@@ -5,6 +5,7 @@ export PR_BODY="$1"
 eventData=$(echo "$PR_BODY" | sed '/^{/,/^}/!d')
 name=$(echo $eventData | jq '.service.name')
 baseService=$(echo $eventData | jq '.service.details.baseService')
+baseVcsName=$(echo $eventData | jq '.service.details.baseVcs.repo.repoName')
 image=$(echo $eventData | jq '.service.details.image.repository')
 tag=$(echo $eventData | jq '.service.details.image.tag')
 
@@ -13,17 +14,24 @@ name="${name%\"}"
 name="${name#\"}"
 baseService="${baseService%\"}"
 baseService="${baseService#\"}"
+baseVcsName="${baseVcsName%\"}"
+baseVcsName="${baseVcsName#\"}"
 image="${image%\"}"
 image="${image#\"}"
 tag="${tag%\"}"
 tag="${tag#\"}"
 
-if [ ! -z "$baseService" -a "$baseService" = "null" ]; then
-  echo "No baseService property found, default to name property"
+if [ ! -z "$baseService" -a "$baseService" = "null" ] && [ ! -z "$baseVcsName" -a "$baseVcsName" = "null" ]; then
+  echo "No base found, default to service name"
   echo "##[set-output name=serviceName;]$name"
 else
-  echo "using baseService property as serviceName"
-  echo "##[set-output name=serviceName;]$baseService"
+  if [ ! -z "$baseVcsName" -a ! "$baseVcsName" = "null" ]; then
+    echo "using baseVcsName property as serviceName"
+    echo "##[set-output name=serviceName;]$baseVcsName"
+  else
+    echo "using baseService property as serviceName"
+    echo "##[set-output name=serviceName;]$baseService"
+  fi
 fi
 echo "##[set-output name=image;]$image"
 echo "##[set-output name=tag;]$tag"
